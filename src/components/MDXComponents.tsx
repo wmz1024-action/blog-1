58cd4eeaab9c42193fa0da5783a8de49988098dc
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-04-25 18:47:45 +0800

    Update MDXComponents.tsx

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,17 +1,3 @@
 import React from "react";
-import Image from "next/image";
-
-const CustomImage = (props) => {
-  return (
-    <Image
-      src={props.src}
-      alt={props.src}
-      width="100%"
-      height="100%"
-      {...props}
-    />
-  );
-};
 export const MDXComponents = {
-  img: CustomImage,
 };
