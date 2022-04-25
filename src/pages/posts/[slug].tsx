29b7e9d28a6c0aed4481c5f168707b4e1942d933
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-04-25 20:02:24 +0800

    Update [slug].tsx

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,13 +1,14 @@
 import { GetStaticPaths, GetStaticProps } from "next";
-
+import { MDXRemote } from "next-mdx-remote";
 import React, { ReactNode } from "react";
 import { getAndSerializePost, getPosts, PostData } from "@/lib/get-posts";
 import Tag from "@/components/Tag";
+import { MDXComponents } from "@/components/MDXComponents";
 import Layout from "@/components/layouts";
 import Link from "next/link";
 import Footer from "@/components/Footer";
 
-const ArticlePage = ({  frontMatter }: PostData): ReactNode => {
+const ArticlePage = ({ mdxSource, frontMatter }: PostData): ReactNode => {
   return (
     <Layout title={frontMatter.title} description={frontMatter.description}>
       <h1>{frontMatter.title}</h1>
@@ -22,18 +23,19 @@ const ArticlePage = ({  frontMatter }: PostData): ReactNode => {
           <a className="meta-back">Back</a>
         </Link>
       </div>
+      <MDXRemote components={MDXComponents} {...mdxSource} />
       <Footer />
     </Layout>
   );
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const {  frontMatter } = await getAndSerializePost(
+  const { mdxSource, frontMatter } = await getAndSerializePost(
     params.slug as string
   );
   return {
     props: {
-     
+      mdxSource,
       frontMatter,
     },
   };
